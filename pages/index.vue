940ec10ebc899653a40e6112d112d1ce1207daae
onyiimesi <onyedikachukwu62@gmail.com>
2024-07-26 18:11:16 +0100

    Ui adjustments

--- pages/index.vue
+++ pages/index.vue
@@ -25,41 +25,7 @@
   </div>
 
   <div class="section pt-12 px-10 md:px-[120px] pb-[120px]">
-    <div class="m-auto">
-      <div class="text-center mb-12">
-        <span class="bg-[#FFF6F5] py-1 px-3 rounded-2xl text-[#5D421F]"
-          >Our Gallery</span
-        >
-        <p class="text-[#101828] text-[36px] font-semibold">Explore Agrinnovation in Lagos</p>
-      </div>
-
-      <div class="flex flex-wrap flex-row gap-4 mb-7">
-        
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      
-        <div class="">
-          <img src="../public/images/media/media2.png" class="rounded-2xl" alt="">
-        </div>
-      </div>
-    </div>
+    <Gallery />
   </div>
 
   <div class="px-10 md:px-[256px] pb-[63px]">
