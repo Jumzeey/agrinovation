c8c083868006f6301d7cebea08ec63a6431bce92
onyiimesi <onyedikachukwu62@gmail.com>
2024-07-29 15:11:14 +0100

    More UI Fix

--- pages/agripreneur/index.vue
+++ pages/agripreneur/index.vue
@@ -37,7 +37,7 @@ const goTo = (slug: any) => {
 
 <template>
     <div class="w-full bg-cover bg-no-repeat bg-[url('/public/images/agripreneur/bg.png')]">
-        <div class="px-0 md:px-[120px]">
+        <div class="mx-0 md:mx-[120px]">
             <Header type="other" />
         </div>
 
