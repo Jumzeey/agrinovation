0607d08b032d0b8e14414ca27ea2b379b3d0b2bb
Jumzeey <31732947+Jumzeey@users.noreply.github.com>
2024-08-29 11:52:35 +0100

    pulling to netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,56 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   components: {
     dirs: [
-      '~/components',
-        {
-          path: '~/components/dashboard/',
-          prefix: 'Dashboard'
-        },
-        {
-          path: '~/components/inputs/',
-          prefix: 'Input'
-        }
-    ]
+      "~/components",
+      {
+        path: "~/components/dashboard/",
+        prefix: "Dashboard",
+      },
+      {
+        path: "~/components/inputs/",
+        prefix: "Input",
+      },
+    ],
   },
-  css: ['~/assets/css/main.css'],
-  plugins:[
-    '~/plugins/toastification.js',
-  ],
+  css: ["~/assets/css/main.css"],
+  plugins: ["~/plugins/toastification.ts"],
+  build: {
+    // Use Vite options instead of Rollup options
+    transpile: ["vue-toastification"], // Example of specifying dependencies to transpile
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
   modules: ["shadcn-nuxt"],
   shadcn: {
     /**
-     * Prefix for all the imported component
+     * Prefix for all the imported components
      */
-    prefix: '',
+    prefix: "",
     /**
      * Directory that the component lives in.
      * @default "./components/ui"
      */
-    componentDir: './components/ui'
+    componentDir: "./components/ui",
   },
   runtimeConfig: {
     public: {
-      apiUrl: process.env.NUXT_PUBLIC_API_URL ?? 'https://backend.agrinnovationsummit.com/api'
-    }
-  }
-})
+      apiUrl:
+        process.env.NUXT_PUBLIC_API_URL ??
+        "https://backend.agrinnovationsummit.com/api",
+    },
+  },
+  vite: {
+    // Vite-specific configuration options
+    // Example: configure Vite to handle specific dependencies
+    optimizeDeps: {
+      include: ["vue-toastification"],
+    },
+  },
+});
